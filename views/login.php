d47e2579f204efda891f386482148ffc207b2df2
ayakade <ayakade13@gmail.com>
2020-10-30 23:30:48 -0700

    Update login.php

--- views/login.php
+++ views/login.php
@@ -1,5 +1,5 @@
 <div class="login">
-    <h2>Login </h2>
+    <h2>Login</h2>
     <form id="login" method="post" action="index.php">
         <input type="hidden" name="controller" value="user" />
 		<input type="hidden" name="action" value="doLogin" />
@@ -7,21 +7,17 @@
         <div class="fieldgroup required">
             <label>Email address</label>
             <input type="text" name="strEmail" value="" />
-            <div class="errorpopup">
-                <p>This field is required</p>
-            </div><!-- .errorpopup -->
         </div><!-- .fieldgroup -->
 
         <div class="fieldgroup required">
             <label>Password</label>
             <input type="text" name="strPassword" value="" />
-            <div class="errorpopup">
-                <p>This field is required</p>
-            </div><!-- .errorpopup -->
         </div><!-- .fieldgroup -->
 
         <input type="submit" value="Login">
     </form>
+
+    <p>Need an account? <strong><a href="index.php?controller=user&action=signup">Sign up now</a></strong></p>
 
 </div><!-- .login / login.php -->
 
