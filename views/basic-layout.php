399c9491005d2efbf7efada0bbe1636ddfffd2c6
ayakade <ayakade13@gmail.com>
2020-10-30 21:43:08 -0700

    Create basic-layout.php

---
+++ views/basic-layout.php
@@ -0,0 +1,11 @@
+<!-- header -->
+<?=$this->headerHTML?>
+
+    <div class="container col-12 col-lg-12">
+        <!-- put contents here -->
+        <?=$this->contentHTML?>
+    </div><!-- .container -->
+
+<!-- footer -->
+<?=$this->footerHTML?>	
+<!-- basic-layout.php -->	
