f3bb3e6f0c7eb75ad8a07efb63314304313bf739
ayakade <ayakade13@gmail.com>
2020-10-30 21:43:35 -0700

    Update PublicController.php

--- controllers/PublicController.php
+++ controllers/PublicController.php
@@ -6,7 +6,16 @@ Class PublicController extends Controller{
     // index page
     public function main()
     {
-        $this->loadRoute("Global", "mainNav"); // load header
+        $this->loadRoute("Global", "header", "headerHTML"); // load header
+        $this->loadRoute("Global", "footer", "footerHTML"); // load footer
+
+        // put contents into heroHTML
+        $this->loadView("views/heroTop.php", 1, "heroHTML");
+
+        // put contents into contentHTML (inside container)
+        $this->loadView("views/city-recommend.php", 1, "contentHTML"); 
+
+        $this->loadView("views/hero-layout.php", 1, "content"); // save the results of this view, into $this->content
 
 		$this->loadLastView("views/main.php"); // final view
 	}
