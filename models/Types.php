f6274ca64c1fdd0d3e31637bd1055edf8b2096d7
ayakade <ayakade13@gmail.com>
2020-10-30 22:28:29 -0700

    Create Types.php

---
+++ models/Types.php
@@ -0,0 +1,30 @@
+<?php
+
+Class Types {
+
+    public function __construct($data)
+	{
+        $this->id = $data["id"];
+        $this->strName = $data["strName"];
+	}
+
+    // get all accomodation type 
+	public static function getAllType()
+	{
+		$types = DB::query("SELECT * from types"); 
+
+        // acting as a factory
+		$typeArray = array(); // empty array to avoid errors when no assignments were found
+		foreach($types as $type)
+		{
+			// create an instance / object for this SPECIFIC 
+			$typeArray[] = new Types($type); // put this  object onto the array
+		}
+
+		// return the list of objects
+		return $typeArray;
+    }
+    
+}
+
+?>
