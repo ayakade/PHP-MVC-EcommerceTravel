2b7e7c28cd22538ef526c2bc4e86698df146790f
ayakade <ayakade13@gmail.com>
2020-10-30 21:43:00 -0700

    Update footer.php

---
+++ views/footer.php
@@ -0,0 +1,30 @@
+<div class="footer col-12 col-lg-12">
+
+    <div class="row">
+        <div class="menu col-12 col-lg-4">
+            <h6>About</h6>
+            <div class="submenu">
+                <a href="#">Company</a>
+                <a href="#">Career</a>
+            </div><!-- .submenu -->
+        </div><!-- .menu -->
+
+        <div class="menu col-12 col-lg-4">
+            <h6>Support</h6>
+            <div class="submenu">
+                <a href="#">Support</a>
+                <a href="#">FQA</a>
+                <a href="#">Contact</a>
+            </div><!-- .submenu -->
+        </div><!-- .menu -->
+
+        <div class="menu col-12 col-lg-4">
+            <i class="fab fa-facebook-square"></i>
+            <i class="fab fa-twitter"></i>
+            <i class="fab fa-instagram"></i>
+        </div><!-- .menu -->
+    </div>
+
+    <p>© 2020 ### All rights reserved</p>
+
+</div><!-- .footer / footer.php -->
