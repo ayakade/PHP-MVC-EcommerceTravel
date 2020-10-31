6f51c07ac5959397cab943669a1afbafe45da1fa
ayakade <ayakade13@gmail.com>
2020-10-30 21:42:38 -0700

    Create hero-layout.php

---
+++ views/hero-layout.php
@@ -0,0 +1,13 @@
+<!-- header -->
+<?=$this->headerHTML?>
+
+<?=$this->heroHTML?>
+
+<div class="container col-12 col-lg-12">
+    <!-- put contents here -->
+    <?=$this->contentHTML?>
+</div><!-- .container -->
+
+<!-- footer -->
+<?=$this->footerHTML?>	
+<!-- basic-layout.php -->	
