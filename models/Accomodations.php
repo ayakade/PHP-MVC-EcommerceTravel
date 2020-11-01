a67b756a7cb3989a60bb43e603278846538980a7
ayakade <ayakade13@gmail.com>
2020-11-01 09:18:53 -0800

    added function search()

--- models/Accomodations.php
+++ models/Accomodations.php
@@ -5,9 +5,13 @@ Class Accomodations {
 	public function __construct($data)
 	{
 		$this->id = $data["id"];
-	}
+    }
+    
+    // find accomodations that match customer's preference
+    public function search()
+    {
 
-
+    }
 }
 
 ?>
