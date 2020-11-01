ec6a408b93d4ec569f9e72c4d5bca9c1035f2345
ayakade <ayakade13@gmail.com>
2020-10-31 20:52:54 -0700

    Update admin-layout.php

--- views/admin-layout.php
+++ views/admin-layout.php
@@ -1,7 +1,7 @@
 <!-- nav -->
 <?=$this->navHTML?>
 
-<div class="container col-12 col-lg-12">
+<div class="container col-12 col-lg-9">
     <!-- put contents here -->
     <?=$this->contentHTML?>
 </div><!-- .container -->
