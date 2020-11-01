f5346cb426c43d98b2ed56a3578c1e3ab1c9d3d2
ayakade <ayakade13@gmail.com>
2020-10-31 22:27:24 -0700

    Update AdminController.php

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -5,20 +5,13 @@ Class AdminController extends Controller {
 	// login page
 	public function login() 
 	{
-		$this->loadRoute("Global", "header", "headerHTML"); // load header
-		$this->loadRoute("Global", "footer", "footerHTML"); // load footer
-		
-		$this->loadView("views/admin-login.php", 1, "contentHTML"); 
-
-        $this->loadView("views/basic-layout.php", 1, "content"); // save the results of this view, into $this->content
-
-		$this->loadLastView("views/admin.php"); // final view
+		$this->loadLastView("views/admin-login.php"); // final view
 	}
 
 	// admin user login
 	public function doLogIn()
 	{
-		$_SESSION["userId"] = Users::LogIn($_POST["strEmail"], $_POST["strPassword"]);
+		$_SESSION["userId"] = Employees::LogIn($_POST["strUsername"], $_POST["strPassword"]);
 
 		if ($_SESSION["userId"])
 		{
@@ -33,35 +26,42 @@ Class AdminController extends Controller {
 	// admin main page after login 
 	public function adminMain() 
 	{
-		$this->loadRoute("Global", "nav", "navHTML"); // load nav
+		$this->loadRoute("Global", "adminNav", "navHTML"); // load nav
 
         $this->loadView("views/admin-main.php", 1, "contentHTML"); 
         $this->loadView("views/admin-layout.php", 1, "content"); // save the results of this view, into $this->content
 
-		$this->loadLastView("views/admin.php"); // final view
+		$this->loadLastView("views/main-admin.php"); // final view
     }
     
     // customer's booking list
     public function bookingList()
     {
-        $this->loadRoute("Global", "nav", "navHTML"); // load nav
+        $this->loadRoute("Global", "adminNav", "navHTML"); // load nav
 
         $this->loadView("views/admin-booking.php", 1, "contentHTML"); 
         $this->loadView("views/admin-layout.php", 1, "content"); // save the results of this view, into $this->content
 
-		$this->loadLastView("views/admin.php"); // final view
+		$this->loadLastView("views/main-admin.php"); // final view
     }
 
     // customer's list
     public function customerList() 
     {
-        $this->loadRoute("Global", "nav", "navHTML"); // load nav
+        $this->loadRoute("Global", "adminNav", "navHTML"); // load nav
 
         $this->loadView("views/admin-customer.php", 1, "contentHTML"); 
         $this->loadView("views/admin-layout.php", 1, "content"); // save the results of this view, into $this->content
 
-		$this->loadLastView("views/admin.php"); // final view
+		$this->loadLastView("views/main-admin.php"); // final view
     }
+
+    // admin logout
+    public function doLogOut()
+    {
+		unset($_SESSION["userId"]);
+		$this->go("admin", "login");
+	}
 }
 
 ?>
