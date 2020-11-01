cefbea2196a6db8eb682a86ed9d711c8b91e6a43
ayakade <ayakade13@gmail.com>
2020-10-31 21:24:22 -0700

    created function bookingList(), customerList()

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -39,7 +39,29 @@ Class AdminController extends Controller {
         $this->loadView("views/admin-layout.php", 1, "content"); // save the results of this view, into $this->content
 
 		$this->loadLastView("views/admin.php"); // final view
-	}
+    }
+    
+    // customer's booking list
+    public function bookingList()
+    {
+        $this->loadRoute("Global", "nav", "navHTML"); // load nav
+
+        $this->loadView("views/admin-booking.php", 1, "contentHTML"); 
+        $this->loadView("views/admin-layout.php", 1, "content"); // save the results of this view, into $this->content
+
+		$this->loadLastView("views/admin.php"); // final view
+    }
+
+    // customer's list
+    public function customerList() 
+    {
+        $this->loadRoute("Global", "nav", "navHTML"); // load nav
+
+        $this->loadView("views/admin-customer.php", 1, "contentHTML"); 
+        $this->loadView("views/admin-layout.php", 1, "content"); // save the results of this view, into $this->content
+
+		$this->loadLastView("views/admin.php"); // final view
+    }
 }
 
 ?>
