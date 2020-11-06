13e40e4e075a08eac131d39c6b5f55a0549de75d
ayakade <ayakade13@gmail.com>
2020-11-05 18:42:51 -0800

    fixed typo

---
+++ views/admin-accommodations.php
@@ -0,0 +1,12 @@
+<div class="adminAccommodationList">
+    <h2>Accommodation list</h2>
+    <a href="#">Add new </a>
+
+    <div class="lists">
+        <a href="index.php?controller=admin&action=accommodation">
+        <div class="list">
+            <h3>Accommodation name</h3>
+        </div><!-- .list -->
+        </a>
+    </div><!-- .lists -->
+</div><!-- .adminAccommodationList / admin-accommodations.php -->
