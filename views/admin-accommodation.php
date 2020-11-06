9384a5c6b38c22ebc99b72be08a3d81e51eacea7
ayakade <ayakade13@gmail.com>
2020-11-05 18:42:47 -0800

    fixed typo

---
+++ views/admin-accommodation.php
@@ -0,0 +1,32 @@
+<div class="adminAccommodation">
+    <h2>Accommodation Page</h2>
+
+    <div class="info">
+        <form method="post" action="index.php" enctype="multipart/form-data">
+            <input type="hidden" name="controller" value="admin" />
+            <input type="hidden" name="action" value="saveAccommodation" />
+            <div class="fieldgroup">
+                <label>Name</label>
+                <input type="text" name="strName" value="" />
+            </div><!-- .fieldgroup -->
+
+            <div class="fieldgroup">
+                <label>Description</label>
+                <input type="text" name="strDescription" value="" />
+            </div><!-- .fieldgroup -->
+
+            <div class="fieldgroup">
+                <label>Image</label>
+                <input type="file" name="image" value="" />
+            </div><!-- .fieldgroup -->
+
+            <div class="fieldgroup">
+                <label>Image alt description</label>
+                <input type="text" name="alt" value="" />
+            </div><!-- .fieldgroup -->
+
+            <input type="submit" value="Save">
+        </form>
+    </div><!-- .info -->
+   
+</div><!-- .adminAccommodation / admin-accommodation.php -->
