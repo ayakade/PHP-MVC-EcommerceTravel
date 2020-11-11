0c0de928e1561c5e8f48ef45e5233068dea52417
ayakade <ayakade13@gmail.com>
2020-11-10 20:38:23 -0800

    Update admin-accommodations.php

--- views/admin-accommodations.php
+++ views/admin-accommodations.php
@@ -1,12 +1,19 @@
 <div class="adminAccommodationList">
     <h2>Accommodation list</h2>
-    <a href="#">Add new </a>
+    <a href="index.php?controller=admin&action=addNew">Add new </a>
 
     <div class="lists">
-        <a href="index.php?controller=admin&action=accommodation">
+        <?php
+        foreach ($this->oAccommodations as $accommodation)
+        {
+        ?>
         <div class="list">
-            <h3>Accommodation name</h3>
+            <a href="index.php?controller=admin&action=accommodation&aId=<?=$accommodation->id?>">
+            <h3><?=$accommodation->strName?></h3>
+            </a>
         </div><!-- .list -->
-        </a>
+        <?php
+        }
+        ?>
     </div><!-- .lists -->
 </div><!-- .adminAccommodationList / admin-accommodations.php -->
