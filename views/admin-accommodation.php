ac050185427b15ad751ceeda9f1081a01e7b291a
ayakade <ayakade13@gmail.com>
2020-11-10 20:38:19 -0800

    Update admin-accommodation.php

--- views/admin-accommodation.php
+++ views/admin-accommodation.php
@@ -5,25 +5,34 @@
         <form method="post" action="index.php" enctype="multipart/form-data">
             <input type="hidden" name="controller" value="admin" />
             <input type="hidden" name="action" value="saveAccommodation" />
+
+            <?php	
+            foreach ($this->oAccommodations as $accommodation)
+            { 
+            ?>
             <div class="fieldgroup">
                 <label>Name</label>
-                <input type="text" name="strName" value="" />
+                <input type="text" name="strName" value="<?=$accommodation->strName?>" />
             </div><!-- .fieldgroup -->
 
             <div class="fieldgroup">
                 <label>Description</label>
-                <input type="text" name="strDescription" value="" />
+                <input type="text" name="strDescription" value="<?=$accommodation->strDescription?>" />
             </div><!-- .fieldgroup -->
 
             <div class="fieldgroup">
                 <label>Image</label>
-                <input type="file" name="image" value="" />
+                <input type="file" name="image" value="<?=$accommodation->image?>" />
             </div><!-- .fieldgroup -->
 
             <div class="fieldgroup">
                 <label>Image alt description</label>
-                <input type="text" name="alt" value="" />
+                <input type="text" name="alt" value="<?=$accommodation->alt?>" />
             </div><!-- .fieldgroup -->
+
+            <?php
+            }
+            ?>
 
             <input type="submit" value="Save">
         </form>
