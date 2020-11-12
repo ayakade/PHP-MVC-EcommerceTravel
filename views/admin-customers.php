79449ae1dbb94e56705dfde32a0359e2929bf330
ayakade <ayakade13@gmail.com>
2020-11-11 22:20:49 -0800

    Create admin-customers.php

---
+++ views/admin-customers.php
@@ -0,0 +1,19 @@
+<div class="customerList col-12 col-lg-12">
+    <h1>Customers List</h1>
+
+    <ol class="list">
+        <?php
+        foreach ($this->oCustomers as $customer)
+        {
+        ?>
+        <li class="customer">
+            <a href="index.php?controller=admin&action=customer&aId=<?=$customer->id?>">
+            <p><?=$customer->name?></p>
+            </a>
+        </li><!-- .customer -->
+        <?php
+        }
+        ?>
+    </ol><!-- .list -->
+
+</div><!-- .customerList / customerList.php -->
