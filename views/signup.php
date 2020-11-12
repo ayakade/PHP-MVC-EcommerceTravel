aafe72eb26dd4068e7ca2810b0178c7d6e1f7d2a
ayakade <ayakade13@gmail.com>
2020-11-11 21:17:17 -0800

    Update signup.php

--- views/signup.php
+++ views/signup.php
@@ -1,20 +1,42 @@
-<div class="signup">
+<div class="signup form">
     <h2>Signup</h2>
-    <form id="login" method="post" action="index.php">
+    <form id="signup" method="post" action="index.php">
         <input type="hidden" name="controller" value="user" />
-		<input type="hidden" name="action" value="doSignup" />
+        <input type="hidden" name="action" value="doSignup" />
+        
+        <div class="fieldgroup required">
+            <label>First name</label>
+            <input type="text" name="strFirstName" value="" placeholder="first name" />
+            <div class="errorpopup">
+                <p>This field is required</p>
+            </div><!-- .error pop up -->
+        </div><!-- .fieldgroup -->
+
+        <div class="fieldgroup required">
+            <label>Last name</label>
+            <input type="text" name="strLastName" value="" placeholder="last name" />
+            <div class="errorpopup">
+                <p>This field is required</p>
+            </div><!-- .error pop up -->
+        </div><!-- .fieldgroup -->
 
         <div class="fieldgroup required">
             <label>Email address</label>
-            <input type="text" name="strEmail" value="" />
+            <input type="text" name="strEmail" value="" placeholder="email address" />
+            <div class="errorpopup">
+                <p>This field is required</p>
+            </div><!-- .error pop up -->
         </div><!-- .fieldgroup -->
 
         <div class="fieldgroup required">
             <label>Password</label>
-            <input type="text" name="strPassword" value="" />
+            <input type="password" name="strPassword" value="" placeholder="password" />
+            <div class="errorpopup">
+                <p>This field is required</p>
+            </div><!-- .error pop up -->
         </div><!-- .fieldgroup -->
 
-        <input type="submit" value="Login">
+        <div class="btn"><input class="cta-secondary" type="submit" value="Sign up"></div>
     </form>
 
     <p>Have an account now? <a href="index.php?controller=user&action=login"><strong>Login Now</strong></a></p>
