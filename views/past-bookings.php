841552ae3ae1399df932943cbb5a6404859d0b5e
ayakade <ayakade13@gmail.com>
2020-11-12 14:15:19 -0800

    renamed file

---
+++ views/past-bookings.php
@@ -0,0 +1,15 @@
+<div class="bookings past-bookings">
+    <h4><?=$this->title?> past trip</h4>
+    <?php
+    foreach ($this->oBookings as $booking)
+    {
+    ?>
+    <div class="booking">
+        <a href="index.php?controller=<?=$this->controller?>&action=booking&bId=<?=$booking->id?>">
+        <p><?=$booking->accommodation?></p>
+        </a>
+    </div><!-- .booking -->
+    <?php
+    }
+    ?>
+</div><!-- .past-bookings / user-past-bookings.php -->
