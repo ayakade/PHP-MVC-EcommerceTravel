d263fe57c8618a972a302510a865ab6c8cb19a37
ayakade <ayakade13@gmail.com>
2020-11-11 22:21:01 -0800

    Delete user-bookings.php

--- views/user-bookings.php
+++
@@ -1,8 +0,0 @@
-<div class="bookingList">
-    <h3>Booking list</h3>
-    <div class="row">
-        <?=$this->upcomingHTML?>
-
-        <?=$this->pastHTML?>
-    </div><!-- .row -->
-</div><!-- .bookingList / user-bookings.php -->
