3d8d28f16aac0cc94c6ee52c1fa83e3a64a238bc
ayakade <ayakade13@gmail.com>
2020-11-12 14:15:28 -0800

    renamed file

---
+++ views/upcoming-bookings.php
@@ -0,0 +1,15 @@
+<div class="bookings upcoming-bookings">
+    <h4><?=$this->title?> upcoming trip</h4>
+    <?php
+    foreach ($this->oBookings as $booking)
+    {
+    ?>
+    <div class="booking">
+        <a href="index.php?controller=<?=$this->controller?>&action=booking&bId=<?=$booking->id?>">
+        <p><?=$booking->accommodation?></p>
+        </a>
+    </div><!-- .booking -->
+    <?php
+    }
+    ?>
+</div><!-- .upcoming-bookings / upcoming-bookings.php -->
