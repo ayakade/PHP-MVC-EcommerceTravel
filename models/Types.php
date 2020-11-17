d8c3417e483c4b8486315063c515d88b67067abf
ayakade <ayakade13@gmail.com>
2020-11-16 17:13:31 -0800

    updated functions

--- models/Types.php
+++ models/Types.php
@@ -5,7 +5,8 @@ Class Types {
     public function __construct($data)
 	{
         $this->id = $data["id"];
-        $this->strName = $data["strName"];
+		$this->strName = $data["strName"];
+		$this->strImage = $data["strImage"];
 	}
 
     // get all accomodation type 
@@ -18,13 +19,29 @@ Class Types {
 		foreach($types as $type)
 		{
 			// create an instance / object for this SPECIFIC 
-			$typeArray[] = new Types($type); // put this  object onto the array
+			$typeArray[] = new Types($type); // put this object onto the array
+		}
+
+		// return the list of objects
+		return $typeArray;
+	}
+	
+	// get spesific type accomodations
+	public static function getType()
+	{
+		$types = DB::query("SELECT * from types"); 
+
+        // acting as a factory
+		$typeArray = array(); // empty array to avoid errors when no assignments were found
+		foreach($types as $type)
+		{
+			// create an instance / object for this SPECIFIC 
+			$typeArray[] = new Types($type); // put this object onto the array
 		}
 
 		// return the list of objects
 		return $typeArray;
     }
-    
 }
 
 ?>
