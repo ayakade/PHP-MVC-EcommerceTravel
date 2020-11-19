ba9cb995d69f96fec25792f24d7f365fb840bd80
ayakade <ayakade13@gmail.com>
2020-11-18 20:44:21 -0800

    Create admin-search.php

---
+++ views/admin-search.php
@@ -0,0 +1,14 @@
+<div class="search">
+    <h1>Find a booking with booking number<h1>
+    <form id="admin-search" method="post" action="index.php">
+        <input type="hidden" name="controller" value="admin" />                
+        <input type="hidden" name="action" value="search" />
+
+        <div class="fieldgroup required">
+            <label>Booking number</label>
+            <input type="text" name="id" value="" placeholder="booking number"/>
+        </div><!-- .fieldgroup -->
+
+        <input class="cta" type="submit" value="Search">
+    </form>
+</div><!-- .search / admin-search.php -->
