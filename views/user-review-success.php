466ab785ff7b1e4bf878c572e53df586ff02db95
ayakade <ayakade13@gmail.com>
2020-11-18 21:04:10 -0800

    Create user-review-success.php

---
+++ views/user-review-success.php
@@ -0,0 +1,9 @@
+<div class="success">
+    <div class="title">
+        <h2>Thank you for writing a review!</h2>
+        <p>Your review has been submitted!</p>
+    </div><!-- .title -->
+
+    <a class="cta" href="index.php?controller=user&action=bookingList">Go back booking list</a>
+
+</div><!-- .success /.user-review-success.php -->
