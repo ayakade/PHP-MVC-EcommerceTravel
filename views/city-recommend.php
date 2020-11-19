78ce9da51a4e7746aa5e0f094e5f65444f5d3b0f
ayakade <ayakade13@gmail.com>
2020-11-18 20:44:14 -0800

    updated

--- views/city-recommend.php
+++ views/city-recommend.php
@@ -1,13 +1,14 @@
 <div class="city-recommend col-12 col-lg-12">
-    <h3>City reccomendation</h3>
+    <h3>Find from destinations</h3>
     <div class="cityList">
         <?php	
         foreach ($this->oCities as $city)
         {
         ?>
-            <div class="city col-6 col-lg-3">
-                <a href="#">
-                <p><?=$city->strName?></p>
+            <div class="city col-6 col-lg-4">
+                <a href="index.php?controller=public&action=city&cId=<?=$city->id?>">
+                    <img src="imgs/<?=$city->strImage?>" alt="<?=$city->strName?>">
+                    <p><?=$city->strName?></p>
                 </a>
             </div><!-- .city -->  
         <?php
