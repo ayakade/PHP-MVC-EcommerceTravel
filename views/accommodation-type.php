b7dd981af2ad1a4455699ff3cee0a6df7e25e85f
ayakade <ayakade13@gmail.com>
2020-11-18 20:41:32 -0800

    Update accommodation-type.php

--- views/accommodation-type.php
+++ views/accommodation-type.php
@@ -1,13 +1,14 @@
 <div class="accommodation-type col-12 col-lg-12">
-    <h3>Choose from accommodation type</h3>
+    <h3>Who is traveling with you?</h3>
     <div class="typeList">
         <?php	
         foreach ($this->oTypes as $type)
         {
         ?>
-            <div class="type col-6 col-lg-3">
-                <a href="#">
-                <p><?=$type->strName?></p>
+            <div class="type col-12 col-lg-4">
+                <a href="index.php?controller=public&action=type&tId=<?=$type->id?>">
+                    <img src="imgs/<?=$type->strImage?>" alt="<?=$type->strName?>">
+                    <p><strong><?=$type->strName?></strong></p>
                 </a>
             </div><!-- .type -->  
         <?php
