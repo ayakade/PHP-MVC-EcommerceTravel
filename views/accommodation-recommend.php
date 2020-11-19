1dfbec343fd1a1550eb0c027ab73c0a4dab9bd80
ayakade <ayakade13@gmail.com>
2020-11-18 20:40:56 -0800

    Create accommodation-recommend.php

---
+++ views/accommodation-recommend.php
@@ -0,0 +1,21 @@
+<div class="accommodation-recommend col-12 col-lg-12">
+    <h3>You will love to travel with our accommodations</h3>
+    <div class="accommodationList">
+        <?php	
+        foreach ($this->oAccommodations as $accommodation)
+        {
+        ?>
+            <div class="accommodation col-12 col-lg-3">
+                <a href="index.php?controller=public&action=accommodation">
+                    <img src="assets/<?=$accommodation->image?>" alt="<?=$accommodation->strName?>">
+                    <h4><?=$accommodation->strName?></h4>
+                    <p><?=$accommodation->city?>, <?=$accommodation->country?></p>
+                    <p><strong>$<?=$accommodation->price?>/night</strong></p>
+                </a>
+            </div><!-- .accommodation -->  
+        <?php
+        }
+        ?>
+    </div><!-- .accommodationList -->
+
+</div><!-- .accommodation-recommend / accommodation-recommend.php -->
