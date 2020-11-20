05f92e262d4e00fd9ff54d65f9edf3908f3a1e86
ayakade <ayakade13@gmail.com>
2020-11-19 23:04:17 -0800

    updated

--- views/admin-new-accommodation.php
+++ views/admin-new-accommodation.php
@@ -1,5 +1,5 @@
-<div class="adminNewAccommodation">
-    <h2>Accommodation Page</h2>
+<div class="accommodation-info">
+    <h2>Add a new accommodation</h2>
 
     <div class="info">
         <form method="post" action="index.php" enctype="multipart/form-data">
@@ -12,8 +12,47 @@
             </div><!-- .fieldgroup -->
 
             <div class="fieldgroup">
+                <label>City</label>
+                <select name="city">
+                <?php	
+                foreach ($this->oCities as $city)
+                { 
+                ?>
+                    <option value="<?=$city->id?>"><?=$city->strName?></option>
+                <?php
+                }
+                ?>
+                </select>
+            </div><!-- .fieldgroup -->
+
+            <div class="fieldgroup">
+                <label>Price</label>
+                <input type="text" name="price" value="" />
+            </div><!-- .fieldgroup -->
+
+            <div class="fieldgroup">
+                <label>Max guest number</label>
+                <input type="text" name="maxGuestNumber" value="" />
+            </div><!-- .fieldgroup -->
+
+            <div class="fieldgroup">
+                <label>Accommodation type</label>
+                <?php	
+                foreach ($this->oTypes as $type)
+                { 
+                ?>
+                <div class="checkbox">
+                    <input type="checkbox" name="type[]" value="<?=$type->id?>" />
+                    <span><?=$type->strName?></span>
+                </div><!-- .checkbox -->
+                <?php
+                }
+                ?>
+            </div><!-- .fieldgroup -->
+
+            <div class="fieldgroup">
                 <label>Description</label>
-                <input type="text" name="strDescription" value="" />
+                <textarea name="strDescription" value=""></textarea>
             </div><!-- .fieldgroup -->
 
             <div class="fieldgroup">
@@ -21,13 +60,8 @@
                 <input type="file" name="image" value="" />
             </div><!-- .fieldgroup -->
 
-            <div class="fieldgroup">
-                <label>Image alt description</label>
-                <input type="text" name="alt" value="" />
-            </div><!-- .fieldgroup -->
-
-            <input type="submit" value="Save">
+            <input class="cta" type="submit" value="Save">
         </form>
     </div><!-- .info -->
    
-</div><!-- .adminNewAccommodation / admin-new-accommodation.php -->
+</div><!-- .accommodation-info / admin-new-accommodation.php -->
