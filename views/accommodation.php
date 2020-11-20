ae93f50689fb1e2ab3849ba2d3bb500631723a51
ayakade <ayakade13@gmail.com>
2020-11-19 23:05:04 -0800

    updated

--- views/accommodation.php
+++ views/accommodation.php
@@ -1,3 +1,61 @@
 <div class="accommodation-info">
-    <h2>this is specific accomodation page</h2>
+    <?php
+    foreach ($this->oAccommodations as $accommodation)
+    {
+    ?>
+    <div class="title">
+        <h2><?=$accommodation->strName?></h2>
+        <p><?=$accommodation->city?>, <?=$accommodation->country?></p>
+    </div><!-- .title --> 
+    <div class="accommodation">
+        <div class="info">
+            <img src="assets/<?=$accommodation->image?>" alt="<?=$accommodation->strName?>">
+
+            <form class="booking" id="booking" method="post" action="index.php">
+                <p>$<strong><?=$accommodation->price?></strong>/night</p>
+
+                <div class="row">
+                    <div class="fieldgroup required">
+                        <label>Check-in</label>
+                        <input type="date" name="checkin" value="" />
+                    </div><!-- .fieldgroup -->
+
+                    <div class="fieldgroup required">
+                        <label>Check-out</label>
+                        <input type="date" name="checkout" value="" />
+                    </div><!-- .fieldgroup -->
+                </div><!-- .row -->
+
+                <div class="fieldgroup required">
+                    <label>Guest number</label>
+                    <input type="text" name="guestNumber" value="" />
+                </div><!-- .fieldgroup -->
+
+                <input class="cta" type="submit" value="Book now">
+            </form>
+            <div class="row">
+                <p><?=$accommodation->strDescription?></p>
+            </div><!-- .row -->
+        </div><!-- .info -->
+    <?php
+    }
+    ?>
+        <div class="reviews">
+            <h3>Reviews</h3>
+            <?php
+            foreach ($this->oReviews as $review)
+            {
+            ?>
+            <div class="review">
+                <div class="row">
+                    <p><strong><?=$review->rates?>/5 <?=$review->name?></strong></p>
+                </div><!-- .row -->
+                <p><?=$review->comments?></p>
+            </div><!-- .review -->
+            <?php
+            }
+            ?>
+        </div><!-- .reviews --> 
+    </div><!-- .accommodation -->
+   
 </div><!-- .accomodation-info / accommodation.php -->
