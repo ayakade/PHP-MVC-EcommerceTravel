0f74863797a76eaa10b2ce98fb7aa22576222b99
ayakade <ayakade13@gmail.com>
2020-11-23 11:08:19 -0800

    Create search-form.php

---
+++ views/search-form.php
@@ -0,0 +1,39 @@
+<form method="get" action="index.php" id="searchForm" class="search">
+    <input type="hidden" name="controller" value="public" />
+    <input type="hidden" name="action" value="search" />
+
+    <div class="fieldgroup required">
+        <label>Location</label>
+        <input type="text" class="location" name="location" value="" placeholder="location">
+        <div class="suggest"></div>
+        <div class="errorpopup">
+            <p>This field is required</p>
+        </div><!-- .error pop up -->
+    </div><!-- end of fieldgroup -->
+
+    <div class="fieldgroup required">
+        <label>Check in</label>
+        <input type="date" id="checkin" name="checkin" value="">
+        <div class="errorpopup">
+            <p>This field is required</p>
+        </div><!-- .error pop up -->
+    </div><!-- end of fieldgroup -->
+
+    <div class="fieldgroup required">
+        <label>Check out</label>
+        <input type="date" id="checkout" name="checkout" value="">
+        <div class="errorpopup">
+            <p>This field is required</p>
+        </div><!-- .error pop up -->
+    </div><!-- end of fieldgroup -->
+
+    <div class="fieldgroup required">
+        <label>Guests</label>
+        <input type="text" id="guest" name="guest" value="" placeholder="guest">
+        <div class="errorpopup">
+            <p>This field is required</p>
+        </div><!-- .error pop up -->
+    </div><!-- end of fieldgroup -->
+
+    <input class="cta" type="submit" value="Search">
+</form>
