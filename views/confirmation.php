6b0d96d38a3d32654eded95f9418d77893ba2d30
ayakade <ayakade13@gmail.com>
2020-11-23 11:08:22 -0800

    Create confirmation.php

---
+++ views/confirmation.php
@@ -0,0 +1,9 @@
+<div class="confirmation">
+    <h2>Your booking is confirmed!</h2>
+    <p>Thank you for booking with us!<br>Your booking number is <strong><?=$_SESSION["bookingId"]?></strong></p>
+
+    <div class="link">
+        <a class="cta-secondary" href="index.php?controller=user&action=bookingList">Go to your account</a>
+        <a class="cta-secondary" href="index.php">Go back home page</a>
+    </div><!-- .link -->
+</div><!-- .confirmation / confirmation.php -->
