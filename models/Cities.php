2ddae76baec4d7af6fe7b303685ce4a40e03e27f
ayakade <ayakade13@gmail.com>
2020-11-23 21:33:31 -0800

    updated

--- models/Cities.php
+++ models/Cities.php
@@ -15,7 +15,25 @@ Class Cities {
 		$cities = DB::query("SELECT * FROM cities"); 
 
         // acting as a factory
-       $cityArray = array(); // set default (empty)
+        $cityArray = array(); // set default (empty)
+
+        foreach($cities as $city)
+        {
+            // create an instance / object for this SPECIFIC city
+            $cityArray[] = new Cities($city); // put this city object onto the array
+        }
+       
+        // return the list of city objects
+        return $cityArray;
+    }
+
+    // get a spacific city 
+	public static function getCity($id)
+	{
+		$cities = DB::query("SELECT * FROM cities WHERE id = ".$id.""); 
+
+        // acting as a factory
+        $cityArray = array(); // set default (empty)
 
         foreach($cities as $city)
         {
@@ -30,10 +48,10 @@ Class Cities {
     // get randam 6 cities 
 	public static function getRandCities()
 	{
-		$cities = DB::query("SELECT * FROM cities ORDER BY RAND() LIMIT 6"); 
+	    $cities = DB::query("SELECT * FROM cities ORDER BY RAND() LIMIT 6"); 
 
         // acting as a factory
-       $cityArray = array(); // set default (empty)
+        $cityArray = array(); // set default (empty)
 
         foreach($cities as $city)
         {
@@ -43,6 +61,24 @@ Class Cities {
        
         // return the list of city objects
         return $cityArray;
-	}
+    }
+    
+    // suggest when user type location
+    public function suggest()
+    {
+        $cities = DB::query("SELECT * FROM cities WHERE strName LIKE '%".$_POST["location"]."%'"); 
 
+        //set default 
+        $html = "<ul>";
+
+        // make list of suggestion
+        while($row = mysqli_fetch_assoc($cities))
+        {
+            $html = $html."<li>".$row["strName"]."</li>";
+        }
+
+        $html = $html."</ul>";
+
+        echo $html;
+    }
 }
