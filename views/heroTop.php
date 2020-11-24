4e00eb932d874150be70fd2bc0e11fbf5d7251ad
ayakade <ayakade13@gmail.com>
2020-11-23 21:24:35 -0800

    updated

--- views/heroTop.php
+++ views/heroTop.php
@@ -3,35 +3,10 @@
         <div class="heroImage"></div>
     </div><!-- .filter -->
 
-    <div class="search">
+    <div class="title">
         <h2>Your next trip</h2>
 
-        <form method="post" action="index.php" id="searchForm">
-            <input type="hidden" name="controller" value="user" />
-            <input type="hidden" name="action" value="doUpdate" />
+        <?=$this->searchHTML?> 
 
-            <div class="fieldgroup">
-                <label>Location</label>
-                <input type="text" name="" value="" placeholder="location">
-            </div><!-- end of fieldgroup -->
-
-            <div class="fieldgroup">
-                <label>Check in</label>
-                <input type="date" name="" value="">
-            </div><!-- end of fieldgroup -->
-
-            <div class="fieldgroup">
-                <label>Check out</label>
-                <input type="date" name="" value="">
-            </div><!-- end of fieldgroup -->
-
-            <div class="fieldgroup">
-                <label>Guests</label>
-                <input type="text" name="" value="" placeholder="guest">
-            </div><!-- end of fieldgroup -->
-
-            <input class="cta" type="submit" value="Search">
-            
-        </form>
-    </div><!-- .search -->
+    </div><!-- .title -->
 </div><!-- .hero / heroTop.php -->
