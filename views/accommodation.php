112b392dbda988b844dd35f9b77067d0ea427e22
ayakade <ayakade13@gmail.com>
2020-11-23 21:32:24 -0800

    updated

--- views/accommodation.php
+++ views/accommodation.php
@@ -1,61 +1,20 @@
-<div class="accommodation-info">
+<div class="accommodations">
     <?php
     foreach ($this->oAccommodations as $accommodation)
     {
     ?>
-    <div class="title">
-        <h2><?=$accommodation->strName?></h2>
-        <p><?=$accommodation->city?>, <?=$accommodation->country?></p>
-    </div><!-- .title --> 
-    <div class="accommodation">
-        <div class="info">
-            <img src="assets/<?=$accommodation->image?>" alt="<?=$accommodation->strName?>">
-
-            <form class="booking" id="booking" method="post" action="index.php">
-                <p>$<strong><?=$accommodation->price?></strong>/night</p>
-
-                <div class="row">
-                    <div class="fieldgroup required">
-                        <label>Check-in</label>
-                        <input type="date" name="checkin" value="" />
-                    </div><!-- .fieldgroup -->
-
-                    <div class="fieldgroup required">
-                        <label>Check-out</label>
-                        <input type="date" name="checkout" value="" />
-                    </div><!-- .fieldgroup -->
-                </div><!-- .row -->
-
-                <div class="fieldgroup required">
-                    <label>Guest number</label>
-                    <input type="text" name="guestNumber" value="" />
-                </div><!-- .fieldgroup -->
-
-                <input class="cta" type="submit" value="Book now">
-            </form>
-            <div class="row">
-                <p><?=$accommodation->strDescription?></p>
-            </div><!-- .row -->
-        </div><!-- .info -->
+        <div class="accommodation">
+            <a href="index.php?controller=public&action=accommodation&aId=<?=$accommodation->id?>">
+                <img src="assets/<?=$accommodation->image?>" alt="<?=$accommodation->strName?>">
+                <div class="title">
+                    <h3><?=$accommodation->strName?></h3>
+                    <p><?=$accommodation->city?>, <?=$accommodation->country?></p>
+                    <p>$<?=$accommodation->price?>/night</p>
+                    <div class="cta">Check detail</div>
+                </div><!-- .title -->
+            </a>
+        </div><!-- .accommodation -->  
     <?php
     }
     ?>
-        <div class="reviews">
-            <h3>Reviews</h3>
-            <?php
-            foreach ($this->oReviews as $review)
-            {
-            ?>
-            <div class="review">
-                <div class="row">
-                    <p><strong><?=$review->rates?>/5 <?=$review->name?></strong></p>
-                </div><!-- .row -->
-                <p><?=$review->comments?></p>
-            </div><!-- .review -->
-            <?php
-            }
-            ?>
-        </div><!-- .reviews --> 
-    </div><!-- .accommodation -->
-   
-</div><!-- .accomodation-info / accommodation.php -->
+</div><!-- .accommodations / accommodation.php-->
