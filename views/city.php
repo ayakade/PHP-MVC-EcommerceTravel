bc5bf4c703ff70fa1841ad0483977e53f03b093f
ayakade <ayakade13@gmail.com>
2020-11-23 21:30:39 -0800

    updated

--- views/city.php
+++ views/city.php
@@ -3,27 +3,11 @@
     foreach ($this->oCities as $city)
     {
     ?>
-    <h2><?=$city->city?></h2>
+    <h2><?=$city->strName?></h2>
     <?php
     }
     ?>
-    <div class="accommodations">
-        <?php	
-        foreach ($this->oCities as $city)
-        {
-        ?>
-        <div class="accommodation">
-            <a href="index.php?controller=public&action=accommodation&aId=<?=$city->id?>">
-                <img src="assets/<?=$city->image?>" alt="<?=$city->strName?>">
-                <div class="title">
-                    <h3><?=$city->strName?></h3>
-                    <p>$<?=$city->price?>/night</p>
-                    <div class="cta">Check detail</div>
-                </div><!-- .title -->
-            </a>
-        </div><!-- .accommodation -->  
-        <?php
-        }
-        ?>
-    </div><!-- .accommodations -->
+
+    <?=$this->resultHTML?>
+    
 </div><!-- .city-accommodation / city.php -->
