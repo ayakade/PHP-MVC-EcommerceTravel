0ae17b676938f6111d046a1e6beaf874e8d31ba7
ayakade <ayakade13@gmail.com>
2020-11-23 21:23:19 -0800

    updated

--- views/type.php
+++ views/type.php
@@ -1,30 +1,13 @@
 <div class="type-accommodation col-12 col-lg-12">
     <?php	
-    foreach ($this->oTypes as $type)
+    foreach ($this->oTitles as $title)
     {
     ?>
-    <h2><?=$type->type?></h2>
+    <h2><?=$title->strName?></h2>
     <?php
     }
     ?>
-    <div class="accommodations">
-        <?php	
-        foreach ($this->oTypes as $type)
-        {
-        ?>
-        <div class="accommodation">
-            <a href="index.php?controller=public&action=accommodation&aId=<?=$type->id?>">
-                <img src="assets/<?=$type->image?>" alt="<?=$type->strName?>">
-                <div class="title">
-                    <h3><?=$type->strName?></h3>
-                    <p><?=$type->city?>, <?=$type->country?></p>
-                    <p>$<?=$type->price?>/night</p>
-                    <div class="cta">Check detail</div>
-                </div><!-- .title -->
-            </a>
-        </div><!-- .accommodation -->  
-        <?php
-        }
-        ?>
-    </div><!-- .accommodations -->
+    
+    <?=$this->resultHTML?>
+
 </div><!-- .city-accommodation / city.php -->
