37c1ba722dc753cb5950b2f1917b74999a07b4b0
ayakade <ayakade13@gmail.com>
2020-11-23 21:22:27 -0800

    added models/BookingDates.php

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ include("models/Accommodations.php");
 include("models/Bookings.php");
 include("models/Categories.php");
 include("models/Reviews.php");
+include("models/BookingDates.php");
 
 include("libs/DB.php");
 include("libs/Errors.php");
