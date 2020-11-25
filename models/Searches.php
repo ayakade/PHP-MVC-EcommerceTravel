1560e58e4e16ab6417e9f65cb76154d285f27f47
ayakade <ayakade13@gmail.com>
2020-11-24 19:58:53 -0800

    created models/Searches.php

---
+++ models/Searches.php
@@ -0,0 +1,20 @@
+<?php 
+
+Class Searches {
+    
+    // check if the date range is available 
+    // reference: https://stackoverflow.com/questions/33174909/room-availability-check-sql
+    public static function search($location, $checkin, $checkout, $guest)
+    {
+        $available = DB::query("SELECT accommodations.id AS id,accommodations.strName, checkin, checkout, maxGuestNumber,  cities.id AS cityId, cities.strName AS city
+        FROM bookings
+        LEFT JOIN accommodations ON accommodations.id = bookings.accommodationId 
+        LEFT JOIN cities ON cities.id = accommodations.cityId
+        WHERE ((checkin >= '".$checkin."' AND checkout <= '".$checkout."') OR (checkin >= '".$checkin."' AND checkout <=  '".$checkout."')) AND cities.strName = '".$location."' AND maxGuestNumber >= '".$guest."'");
+
+        // if there's no match, available
+        if($available =="") {
+            return true;
+        }
+    }
+}
