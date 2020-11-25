0b301131064c4e65d82b1c7eff513cda7a1e41bf
ayakade <ayakade13@gmail.com>
2020-11-24 20:00:12 -0800

    updated

--- views/admin-accommodation.php
+++ views/admin-accommodation.php
@@ -1,41 +1,76 @@
-<div class="adminAccommodation">
-    <h2>Accommodation Page</h2>
+<div class="accommodation-info">
+    <?php	
+    foreach ($this->oAccommodations as $accommodation)
+    { 
+    ?>
+    <h2><?=$accommodation->strName?></h2>
 
     <div class="info">
-        <form method="post" action="index.php" enctype="multipart/form-data">
+        <form id="update" method="post" action="index.php" enctype="multipart/form-data">
             <input type="hidden" name="controller" value="admin" />
-            <input type="hidden" name="action" value="saveAccommodation" />
+            <input type="hidden" name="action" value="update" />
 
-            <?php	
-            foreach ($this->oAccommodations as $accommodation)
-            { 
-            ?>
             <div class="fieldgroup">
                 <label>Name</label>
                 <input type="text" name="strName" value="<?=$accommodation->strName?>" />
             </div><!-- .fieldgroup -->
 
             <div class="fieldgroup">
+                <label>* City</label>
+                <select name="city">
+                <?php	
+                foreach ($this->oCities as $city)
+                { 
+                ?>
+                    <option value="<?=$city->id?>"><?=$city->strName?></option>
+                <?php
+                }
+                ?>
+                </select>
+            </div><!-- .fieldgroup -->
+
+            <div class="fieldgroup">
+                <label>Price</label>
+                <input type="text" name="price" value="<?=$accommodation->price?>" />
+            </div><!-- .fieldgroup -->
+
+            <div class="fieldgroup">
+                <label>Max guest number</label>
+                <input type="text" name="maxGuestNumber" value="<?=$accommodation->maxGuestNumber?>" />
+            </div><!-- .fieldgroup -->
+
+            <div class="fieldgroup">
+                <label>* Accommodation type</label>
+                <?php	
+                foreach ($this->oTypes as $type)
+                { 
+                ?>
+                <div class="checkbox">
+                    <input type="checkbox" name="type[]" value="<?=$type->id?>"/>
+                    <span><?=$type->strName?></span>
+                </div><!-- .checkbox -->
+                <?php
+                }
+                ?>
+            </div><!-- .fieldgroup -->
+
+            <div class="fieldgroup">
                 <label>Description</label>
-                <input type="text" name="strDescription" value="<?=$accommodation->strDescription?>" />
+                <textarea name="strDescription" value="<?=$accommodation->strDescription?>"><?=$accommodation->strDescription?></textarea>
             </div><!-- .fieldgroup -->
 
             <div class="fieldgroup">
                 <label>Image</label>
+                <img src="assets/<?=$accommodation->image?>" alt="<?=$accommodation->strName?>">
                 <input type="file" name="image" value="<?=$accommodation->image?>" />
             </div><!-- .fieldgroup -->
 
-            <div class="fieldgroup">
-                <label>Image alt description</label>
-                <input type="text" name="alt" value="<?=$accommodation->alt?>" />
-            </div><!-- .fieldgroup -->
-
-            <?php
-            }
-            ?>
-
-            <input type="submit" value="Save">
+            <input class="cta" type="submit" value="Save">
         </form>
     </div><!-- .info -->
+
+    <?php
+    }
+    ?>
    
-</div><!-- .adminAccommodation / admin-accommodation.php -->
+</div><!-- .accommodation-info / admin-accommodation.php -->
