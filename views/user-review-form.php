c432d9a0e2fb263f5a772a8ba4681253fdc237f4
ayakade <ayakade13@gmail.com>
2020-11-24 19:59:13 -0800

    Create user-review-form.php

---
+++ views/user-review-form.php
@@ -0,0 +1,35 @@
+<?php
+foreach ($this->oBookings as $booking)
+{
+?>
+<div class="reviews">
+    <h3>Write a review</h3>
+    <form id="review" method="post" action="index.php">
+        <input type="hidden" name="controller" value="user" />                
+        <input type="hidden" name="action" value="writeReview" />
+
+        <input type="hidden" name="accommodationId" value="<?=$booking->accommodationId?>">
+        <input type="hidden" name="userId" value="<?=$_SESSION["userId"]?>">
+
+        <div class="fieldgroup required">
+            <label>Rate</label>
+            <select name="rates">
+                <option value="1">1 (Bad)</option>
+                <option value="2">2 (Not good)</option>
+                <option value="3">3 (okay)</option>
+                <option value="4">4 (good)</option>
+                <option value="5">5 (great)</option>
+            </select>
+        </div><!-- .fieldgroup -->
+
+        <div class="fieldgroup required">
+            <label>Comment</label>
+            <textarea name="comments" value=""></textarea>
+        </div><!-- .fieldgroup -->
+
+        <input class="cta" type="submit" value="Write a review">
+    </form>
+</div><!-- .reviews -->
+<?php
+}
+?>
