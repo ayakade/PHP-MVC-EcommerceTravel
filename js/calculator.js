8516895e6663ede75d5efae25255445538d61d60
ayakade <ayakade13@gmail.com>
2020-11-24 20:01:23 -0800

    updated

--- js/calculator.js
+++ js/calculator.js
@@ -1,102 +1,69 @@
 // calcurate fee
 document.addEventListener("DOMContentLoaded", function() {
 
-    // $("#orderTotal").click(function () {
-    //     calculatePrice();
-    // });
-
-    // function calculatePrice () {
-    //     var price = $('#price').val();
-    //     console.log(price);
-
-    //     var guest = $('#guest').val();
-    //     console.log(guest);
-
-    //     // how long stay
-    //     // reference: https://stackoverflow.com/questions/38701847/how-can-i-convert-a-date-into-an-integer
-    //     var checkinData = $('#checkin').val();
-    //     var checkin = Number(new Date(checkinData));
-    //     console.log(checkin);
-    //     var checkoutData = $('#checkout').val();
-    //     var checkout = Number(new Date(checkoutData));
-    //     console.log(checkout);
-    //     var totalStay = (checkout - checkin)/86400000;
-    //     console.log(totalStay);
-
-    //     // sub total
-    //     var subtotal = $('#subtotal'); 
-    //     var subtotalPrice = price * totalStay;
-    //     console.log(subtotalPrice);
-    //     subtotal.val("$"+ subtotalPrice);
-
-    //     // tax (5%)
-    //     var tax =$('#tax');
-    //     var taxPrice = subtotalPrice * 0.05;
-    //     console.log(taxPrice);
-    //     tax.val("$"+ taxPrice);
-
-    //     // total
-    //     var total =$('#total');
-    //     var totalPrice = subtotalPrice + taxPrice;
-    //     console.log(totalPrice);
-    //     total.val("$"+ totalPrice);
-    // };
-
-    // reference: https://stackoverflow.com/questions/5947501/jquery-auto-calculation-based-on-field
     $(function() {
         var price = $('#price').val();
-        console.log(price);
-
         var guest = $('#guest').val();
-        console.log(guest);
+        // console.log(guest);
+         
+        // if there's already dates and guest 
+        if($('#guest').val()) {
+            // how long stay
+            // reference: https://stackoverflow.com/questions/38701847/how-can-i-convert-a-date-into-an-integer
+            var checkinData = $('#checkin').val();
+            var checkin = Number(new Date(checkinData));
+            // console.log(checkin);
+            var checkoutData = $('#checkout').val();
+            var checkout = Number(new Date(checkoutData));
+            // console.log(checkout);
+            var totalStay = (checkout - checkin)/86400000;
+            // console.log(totalStay);
 
-        // how long stay
-        // reference: https://stackoverflow.com/questions/38701847/how-can-i-convert-a-date-into-an-integer
-        var checkinData = $('#checkin').val();
-        var checkin = Number(new Date(checkinData));
-        console.log(checkin);
-        var checkoutData = $('#checkout').val();
-        var checkout = Number(new Date(checkoutData));
-        console.log(checkout);
-        var totalStay = (checkout - checkin)/86400000;
-        console.log(totalStay);
+            // sub total
+            var subtotal = $('#subtotal'); 
+            var subtotalPrice = price * totalStay;
+            // console.log(subtotalPrice);
+            subtotal.val(subtotalPrice);
 
-        // sub total
-        var subtotal = $('#subtotal'); 
-        var subtotalPrice = price * totalStay;
-        console.log(subtotalPrice);
-        subtotal.val(subtotalPrice);
+            // tax (5%)
+            var tax =$('#tax');
+            var taxPrice = subtotalPrice * 0.05;
+            // console.log(taxPrice);
+            tax.val(taxPrice);
 
-        // tax (5%)
-        var tax =$('#tax');
-        var taxPrice = subtotalPrice * 0.05;
-        console.log(taxPrice);
-        tax.val(taxPrice);
+            // total
+            var total =$('#total');
+            var totalPrice = subtotalPrice + taxPrice;
+            // console.log(totalPrice);
+            total.val(totalPrice);
 
-        // total
-        var total =$('#total');
-        var totalPrice = subtotalPrice + taxPrice;
-        console.log(totalPrice);
-        total.val(totalPrice);
+        } else {
+            // calculate price when type guest number
+            $('#guest').keyup(function() {
+                // how long stay
+                // reference: https://stackoverflow.com/questions/38701847/how-can-i-convert-a-date-into-an-integer
+                var checkinData = $('#checkin').val();
+                var checkin = Number(new Date(checkinData));
 
-        // $(document).ready(function() { 
-            
-        // });
+                var checkoutData = $('#checkout').val();
+                var checkout = Number(new Date(checkoutData));
 
-        // sub total
-        $('#subtotal').change(function(){
-            $('#subtotal').val(subtotalPrice);
-        });
+                var totalStay = (checkout - checkin)/86400000;
 
-        // tax
-        $('#tax').change(function(){
-            $('#tax').val(taxPrice);
-        });
+                // sub total
+                var subtotalPrice = price * totalStay;
 
-        // total
-        $('#total').change(function(){
-            $('#total').val(totalPrice);
-        });
+                // tax (5%)
+                var taxPrice = subtotalPrice * 0.05;
+
+                // total
+                var totalPrice = subtotalPrice + taxPrice;
+
+                $('#subtotal').val(subtotalPrice);
+                $('#tax').val(taxPrice);
+                $('#total').val(totalPrice);
+            });
+        }
     });
 });
 
