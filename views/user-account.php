bfd2a8a5f93deb750a7cd9d0f81962be08c792e0
ayakade <ayakade13@gmail.com>
2020-11-28 21:10:39 -0800

    updated

--- views/user-account.php
+++ views/user-account.php
@@ -4,27 +4,27 @@
     <form method="post" action="index.php" id="accountInfo">
         <input type="hidden" name="controller" value="user" />
 		<input type="hidden" name="action" value="doUpdate" />
-        <input type="hidden" name="id" value="<?=$this->oUsers->id?>"/>
+        <input type="hidden" name="id" value="<?=$this->oUser->id?>"/>
         
 
         <div class="fieldgroup">
             <label>First name</label>
-            <input type="text" name="strFirstName" value="<?=$this->oUsers->strFirstName?>">
+            <input type="text" name="strFirstName" value="<?=$this->oUser->strFirstName?>">
         </div><!-- end of fieldgroup -->
 
         <div class="fieldgroup">
             <label>Last name</label>
-            <input type="text" name="strLastName" value="<?=$this->oUsers->strLastName?>">
+            <input type="text" name="strLastName" value="<?=$this->oUser->strLastName?>">
         </div><!-- end of fieldgroup -->
 
         <div class="fieldgroup">
             <label>Email address</label>
-            <input type="text" name="strEmail" value="<?=$this->oUsers->strEmail?>">
+            <input type="text" name="strEmail" value="<?=$this->oUser->strEmail?>">
         </div><!-- end of fieldgroup -->
 
         <div class="fieldgroup">
             <label>PhoneNumber</label>
-            <input type="text" name="strPhoneNumber" value="<?=$this->oUsers->strPhoneNumber?>">
+            <input type="text" name="strPhoneNumber" value="<?=$this->oUser->strPhoneNumber?>">
         </div><!-- end of fieldgroup -->
 
         <div class="btn"><input class="cta" type="submit" value="Update info"></div>
