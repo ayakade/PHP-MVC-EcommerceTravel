d0331c6f70dbce206224b304806860bc2716d53a
ayakade <ayakade13@gmail.com>
2020-11-29 09:57:07 -0800

    updated

--- models/Categories.php
+++ models/Categories.php
@@ -40,6 +40,51 @@ Class Categories {
         // return the list of objects
         return $accommodationArray;
     }
+
+    // get all type accomodations categorized
+	public static function getType($id) {
+
+        // // set the default(when no category is checked(empty))
+        // $categories = false;
+
+        // $sql = DB::query("SELECT * FROM accommodationTypes WHERE accommodationId = ".$id.""); 
+
+        // // print_r($sql);
+
+        // $results = mysqli_query(DB::connect(), $sql);   
+
+        // while($data = mysqli_fetch_assoc($results))
+        // {
+        //     $categories[$data["typeID"]] = true;
+        // }
+
+        // print_r($categories);
+
+        // return $categories;
+    
+        $categories = DB::query("SELECT accommodations.id AS id, types.id AS typeID, types.strName AS type, accommodations.strName, strDescription, maxGuestNumber, price, cities.strName AS city, countries.strName AS country, accommodationImages.strFirstImage AS image 
+        FROM accommodationTypes
+        LEFT JOIN types ON accommodationTypes.typeId = types.id
+        LEFT JOIN accommodations ON accommodationTypes.accommodationId = accommodations.id
+        LEFT JOIN cities ON cities.id = accommodations.cityId
+        LEFT JOIN accommodationImages ON accommodationImages.id = accommodations.accommodationImageId
+        LEFT JOIN countries ON countries.id = cities.countryId
+        WHERE accommodations.id = ".$id.""); 
+
+        // acting as a factory
+        $categoryArray = array(); // set default(empty)
+
+        foreach($categories as $category)
+        {
+        // create an instance / object for this SPECIFIC 
+        $categoryArray[] = new Categories($category); // put this  object onto the array
+        }
+        
+        print_r($categoryArray);
+
+        // return the list of objects
+        return $categoryArray;
+    }
 }
 
 ?>
